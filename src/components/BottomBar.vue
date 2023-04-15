4cb67b0401f8dec5a7b698ca36aaccaf85f45d73
Erik Piet <admin@erikpiet.nl>
2023-04-15 11:16:29 +0200

    lege regels weggehaald

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -5,17 +5,14 @@
     >
       <v-btn>
         <v-icon>mdi-wrench</v-icon>
-
         <span>Active task</span>
       </v-btn>
       <v-btn>
         <v-icon>mdi-magnify</v-icon>
-
         <span>Search</span>
       </v-btn>
       <v-btn>
         <v-icon>mdi-information</v-icon>
-
         <span>Information</span>
       </v-btn>
     </v-bottom-navigation>
